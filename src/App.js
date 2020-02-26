8e01b4bfb5f9f09cfcaa723be1aa8d348eac0534
joeharwood96 <joseph.harwood@ibm.com>
2020-02-26 22:00:49 +0000

    Added navigation bar and home-page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Pages
+import HomePage from './pages/home-page';
+
+// Components
+import Navigation from './components/navigation';
+
+
+
+const colorScheme = {
+  primary: '#525E67',
+  secondary: '#FCB03A',
+  tertiary: '#f5f5f5'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation colorScheme={colorScheme} />
+      <HomePage colorScheme={colorScheme}/>
     </div>
   );
 }
---
+++ src/components/navigation.jsx
@@ -0,0 +1,107 @@
+import React, {useState, useLayoutEffect} from 'react';
+import styled from 'styled-components';
+
+// MUI
+import Link from '@material-ui/core/Link';
+
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+}
+
+export default function Navigation(props) {
+    const {
+        colorScheme 
+    } = props;
+
+    const NavWrapper = styled.section`
+        display: flex;
+        justify-content: space-between;
+        background-color: ${props.colorScheme.tertiary};
+        width: 100vw;
+    `;
+
+    const NavTitle = styled.h2`
+        font-size: 3em;
+        margin: 0;
+        padding: 1rem;
+        margin-left: 10rem;
+        color: ${props.colorScheme.secondary};
+
+        @media(max-width: 1408px) {
+            margin-left: 1rem;
+        }
+
+        @media(max-width: 902px) {
+            font-size: 2em;
+            margin-top: 0.5rem;
+        }
+
+        @media(max-width: 660px) {
+            font-size: 1.5em;
+            margin-top: 1.2rem;
+        }
+
+        @media(max-width: 545px) {
+            font-size: 1.2em;
+            margin-top: 1.4rem;
+        }
+
+        @media(max-width: 500px) {
+            font-size: 0.9em;
+            margin-top: 1.5rem;
+        }
+    `;
+
+    const LinkWrapper = styled.section`
+        color: ${colorScheme.primary};
+        padding: 2.5rem;
+        margin-right: 10rem;
+
+        @media(max-width: 1408px) {
+            margin-right: 1rem;
+        }
+    `;
+
+    const MenuButton = styled.button`
+        color: ${colorScheme.primary};
+        font-size: 1em;
+        height: 3em;
+        width: 4em;
+        margin: 1.5em;
+        padding: 0.5em 0.5em;
+        border: 2px solid ${colorScheme.primary};
+        border-radius: 3px;
+    `;
+
+    const [windowWidth, ] = useWindowSize();
+    return (
+        <NavWrapper>
+            <NavTitle>Joe Harwood <span style={{color: colorScheme.primary}}>/ Software Engineer</span></NavTitle>
+            {
+                windowWidth < 1120 ?
+                <MenuButton>MENU</MenuButton>
+                :
+                <LinkWrapper>
+                    <Link color="inherit" href="/" style={{marginLeft: '1rem'}}>
+                        Projects
+                    </Link>
+                    <Link color="inherit" href="/about-me" style={{marginLeft: '1rem'}}>
+                        About Me
+                    </Link>
+                    <Link color="inherit" href="https://drive.google.com/file/d/12XBR7IokX0VPABrBL0G2CSUovcd4LtuT/view" style={{marginLeft: '1rem'}}>
+                        Resume
+                    </Link>
+                </LinkWrapper>
+            }
+        </NavWrapper>
+    );
+}
