d8c78bddb42681095c182495f4290bc939b43008
joeharwood96 <joseph.harwood@ibm.com>
2020-03-19 11:17:43 +0000

    Updates

---
+++ precache-manifest.2393904cb223c20a11cc796e5c0e0d27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf70f0bdb69d82c53764af69deb7c770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e737cd5e4af6880131",
+    "url": "/static/css/main.dff52559.chunk.css"
+  },
+  {
+    "revision": "a9a4d68a3bc16ba564b7",
+    "url": "/static/js/2.09637549.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.09637549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e737cd5e4af6880131",
+    "url": "/static/js/main.f4802c3b.chunk.js"
+  },
+  {
+    "revision": "49eb3469a23f16e344d5",
+    "url": "/static/js/runtime-main.cd8be92d.js"
+  },
+  {
+    "revision": "bd54ccd422c1f41c4f4fa1ddb154b848",
+    "url": "/static/media/crownLogo.bd54ccd4.svg"
+  },
+  {
+    "revision": "b99ab005c78307957c85f700c173788f",
+    "url": "/static/media/tourdemancLogo.b99ab005.png"
+  },
+  {
+    "revision": "04a4b05a3a364758d460fb1ec66e4989",
+    "url": "/static/media/vehicleLogo.04a4b05a.svg"
+  }
+]);
